13407ee75f083f56d7980bfe42d53598d14807f3
pedr0-Rafae1 <rs9981342@gmail.com>
2026-09-17 10:08:57 -0300

    Add files via upload

---
+++ 3 Bimestre/TodoList - IMC/app/controllers/controller.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../models/tarefa.php'
+
+class TarefaController{
+    private $tarefaModel;
+
+    public function __construct(){
+        $this->tarefaModel = new Tarefas();
+    }
+
+    public function criar(){
+        if(isset($_POST['descricao']) && !empty(trims($_POST['descricao']))){
+            $this->tarefaModel->criar($_POST['descricao']);
+        }
+
+        header("Location: index.php");
+    }
+
+    public function excluir(){
+
+        if(isset[$_GET['delete']]){
+            $this->tarefaModel->excluir($_GET['delete']);
+        }
+
+        header("Location: index.php");
+    }
+
+    public function index(){
+        $tarefa = $this->tarefaModel->listar();
+        include __DIR__ . '/../views/listar';
+    }
+
+}
+
+?>
---
+++ 3 Bimestre/TodoList - IMC/app/models/tarefa.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class Tarefas{
+    private $conn;
+public function __construct(){
+    $db = new Database();
+    $this->conn = $db->conectar();
+}
+
+public function criar($descricao){
+    $descricao = $this-> conn-> real_escape_string($descricao);
+    $sql = "INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+    return $this -> conn -> query($sql);
+}
+
+public function excluir($id){
+    $id = intval($id);
+    $sql = "DELETE FROM tarefas WHERE id = ?";
+    return $this -> conn -> query($sql);
+}
+
+    public function listar(){
+        $tarefa = [];
+        $sql = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
+        $resultado = $this -> conn -> query($sql);
+
+        if( $resultado -> num_rows > 0){
+
+            while($row = $resultado -> fetch_assoc()){
+                $tarefa[] = $row;
+            }
+
+        return $tarefa;
+
+        }
+    }
+};
